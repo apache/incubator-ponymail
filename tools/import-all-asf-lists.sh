823230a4660913d23bf4eee906e98ce4e30f4efb
pctony <tony@pc-tony.com>
2015-09-09 23:30:06 +0100

    Add a script to bludgeonly add all ASF lists history to the archive.

---
+++ tools/import-all-asf-lists.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for tlp in `ls -1 /mbox/`; do 
+  for list in `ls -1 /mbox/${tlp}/`; do
+    python ./import_mod_mbox.py --source /mbox/${tlp}/${list} --recursive 1 --ext "" --lid "<${list}.${tlp}.apache.org>" ; 
+  done
+done
+
+
+
+
+
