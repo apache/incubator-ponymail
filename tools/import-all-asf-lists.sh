f2742f702efdd20b97e075f7eaef95d04ff6333a
humbedooh <humbedooh@apache.org>
2015-09-10 16:37:03 +0200

    don't try to doubly import foo@foo

--- tools/import-all-asf-lists.sh
+++ tools/import-all-asf-lists.sh
@@ -3,8 +3,10 @@ DIR=$1
 for tlp in `ls -1 ${DIR}`; do 
   for list in `ls -1 ${DIR}/${tlp}/ | sort -g`; do
     if [ "${tlp}" -ne "DONE" ]; then
-      printf "\n*** Importing ${tlp}/${list} ***\n"
-      python ./import-mbox.py --source {DIR}/${tlp}/${list} --ext "" --lid "<${list}.${tlp}.apache.org>" ;
+        if [ "${tlp}" -ne "${list}" ]; then
+        printf "\n*** Importing ${tlp}/${list} ***\n"
+          python ./import-mbox.py --source {DIR}/${tlp}/${list} --ext "" --lid "<${list}.${tlp}.apache.org>" ;
+        fi
     fi
   done
 done
