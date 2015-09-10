14af7d822e2cc33fb2658fcedc0b0e060f361787
humbedooh <humbedooh@apache.org>
2015-09-10 16:26:50 +0200

    make import-asf work with variable dir

--- tools/import-all-asf-lists.sh
+++ tools/import-all-asf-lists.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
-for tlp in `ls -1 /mbox/`; do 
-  for list in `ls -1 /mbox/${tlp}/ | sort -g`; do
+DIR=$1
+for tlp in `ls -1 ${DIR}`; do 
+  for list in `ls -1 ${DIR}/${tlp}/ | sort -g`; do
     printf "\n*** Importing ${tlp}/${list} ***\n"
-    python ./import-mbox.py --source /mbox/${tlp}/${list} --ext "" --lid "<${list}.${tlp}.apache.org>" ; 
+    python ./import-mbox.py --source {DIR}/${tlp}/${list} --ext "" --lid "<${list}.${tlp}.apache.org>" ; 
   done
 done
 
