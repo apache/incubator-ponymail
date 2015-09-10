1f12c65bcd491540f8c46724e4a4487463b7becb
humbedooh <humbedooh@apache.org>
2015-09-10 16:25:45 +0200

    fix asf importer

--- tools/import-all-asf-lists.sh
+++ tools/import-all-asf-lists.sh
@@ -3,7 +3,7 @@
 for tlp in `ls -1 /mbox/`; do 
   for list in `ls -1 /mbox/${tlp}/ | sort -g`; do
     printf "\n*** Importing ${tlp}/${list} ***\n"
-    python ./import_mod_mbox.py --source /mbox/${tlp}/${list} --recursive 1 --ext "" --lid "<${list}.${tlp}.apache.org>" ; 
+    python ./import-mbox.py --source /mbox/${tlp}/${list} --ext "" --lid "<${list}.${tlp}.apache.org>" ; 
   done
 done
 
