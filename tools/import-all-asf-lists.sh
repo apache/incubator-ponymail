94f878ad2c52a32d0721527dce671d5eedd8a837
humbedooh <humbedooh@apache.org>
2015-09-10 16:46:39 +0200

    bit more wizardry

--- tools/import-all-asf-lists.sh
+++ tools/import-all-asf-lists.sh
@@ -3,7 +3,8 @@ DIR=$1
 for tlp in `ls -1 ${DIR}`; do 
   for list in `ls -1 ${DIR}/${tlp}/ | sort -g`; do
     if [ "${tlp}" != "DONE" ]; then
-        if [ "${tlp}" != "${list}" ]; then
+        TLPN=`echo ${tlp} | cut -d. -f1`
+        if [ "${TLPN}" != "${list}" ]; then
         printf "\n*** Importing ${tlp}/${list} ***\n"
           python ./import-mbox.py --source ${DIR}/${tlp}/${list} --ext "" --lid "<${list}.${tlp}.apache.org>" ;
         fi
