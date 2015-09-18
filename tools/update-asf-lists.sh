b32c1f75d5b041d00e834ae0860ed7e666dbff13
humbedooh <humbedooh@apache.org>
2015-09-18 12:33:24 +0200

    don't be recursive, importer does that for us :)

--- tools/update-asf-lists.sh
+++ tools/update-asf-lists.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 DIR=$1
 for tlp in `ls -1 ${DIR}`; do 
-  for list in `ls -1 ${DIR}/${tlp}/ | sort -g`; do
     if [ "${tlp}" != "DONE" ]; then
         TLPN=`echo ${tlp} | cut -d. -f1`
-        LETTER=`echo ${TLPN} | cut -c1-1`
-        if [ "${LETTER}" != "a" ]; then
-          if [ "${TLPN}" != "${list}" ]; then
-            printf "\n*** Updating ${TLPN} ***\n"
-            python import-mbox.py --source "http://mail-archives.eu.apache.org/mod_mbox/" --quick --mod-mbox --project ${TLPN};
-          fi
+        if [ "${TLPN}" != "${list}" ]; then
+          printf "\n*** Updating ${TLPN} ***\n"
+          python import-mbox.py --source "http://mail-archives.eu.apache.org/mod_mbox/" --quick --mod-mbox --project ${TLPN};
         fi
-      fi
-  done
+    fi
 done
 
