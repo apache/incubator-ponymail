ab55374ca2aeaa2af33efddb318d53a095eec0be
humbedooh <humbedooh@apache.org>
2015-09-19 07:57:34 +0200

    Update word cloud algo

--- site/js/wordcloud.js
+++ site/js/wordcloud.js
@@ -44,7 +44,7 @@ function wordCloud(hash, width, height) {
     for (var n in hashSorted) {
         var word = hashSorted[n]
         var size = 0;
-        var expected_area = ( Math.sqrt(hash[word]) / total ) * (space*0.7)
+        var expected_area = ( Math.sqrt(hash[word]) / total ) * (space*0.9)
         console.log(expected_area)
         var w
         for (var s = 100; s > 0; s--) {
@@ -61,6 +61,8 @@ function wordCloud(hash, width, height) {
             }
             svg.removeChild(txt)
         }
+        
+        
         var txt = document.createElementNS(svgNS, "text");
         txt.setAttribute("font-size", size + "px")
         txt.setAttribute("x", "0")
@@ -75,39 +77,71 @@ function wordCloud(hash, width, height) {
         var theta = 50;
         var prop = height/width
         var popped = false
-        while( theta < 150*Math.PI) {
-            var nx = (width/2) - (w.width/2) + ((theta * Math.cos(theta)))
-            var ny = (height/2) + ((theta * Math.sin(theta))*prop)
-            txt.setAttribute("x", nx)
-            txt.setAttribute("y", ny)
-            theta = theta + increment;
-            increment *= 0.995
-            if (increment < 0.05) {
-                increment = 0.05
-            }
-            
-            var it = false
-            if (nx > 10 && nx < (width-w.width-12) && (ny > w.height+10 && ny < height-12)) {
-                //code
-            } else {
-                continue
-            }
-            for (var b in boxes) {
-                if (intersects(txt, boxes[b], nx, ny)) {
-                    //alert("!")
-                    it = true
-                    break
+        
+        // Try with random placement
+        if (!popped) {
+            for(var n = 0; n < 50; n++) {
+                var nx = (width-w.width) * Math.random()
+                var ny = ((height-w.height) * Math.random()) + w.height
+                txt.setAttribute("x", nx)
+                txt.setAttribute("y", ny)
+                size *= 0.9
+                txt.setAttribute("font-size", size + "px")
+                if (!(nx > 8 && nx < (width-w.width-16) && (ny > w.height+8 && ny < height-8))) continue
+                var it = false
+                for (var b in boxes) {
+                    if (intersects(txt, boxes[b], nx, ny)) {
+                        it = true
+                        break
+                    }
                 }
+                if (!it) {
+                    popped = true
+                    break
+                } 
             }
-            if (!it) {
-                popped = true
-                
-                break
-            } 
         }
         
-        boxes.push(txt)
-        w = txt.getBBox();
+        // Didn't work? Spiral time!
+        if (!popped) {
+            for (var ss = size; ss > 10; ss -= 3) {
+                txt.setAttribute("font-size", size + "px")
+                while( theta < 60*Math.PI) {
+                    var nx = (width/2) - (w.width/2) + ((theta * Math.cos(theta)))
+                    var ny = (height/2) + ((theta * Math.sin(theta))*prop)
+                    txt.setAttribute("x", nx)
+                    txt.setAttribute("y", ny)
+                    theta = theta + increment;
+                    increment *= 0.995
+                    if (increment < 0.05) {
+                        increment = 0.05
+                    }
+                    var it = false
+                    if (!(nx > 8 && nx < (width-w.width-16) && (ny > w.height+8 && ny < height-8))) continue
+                    for (var b in boxes) {
+                        if (intersects(txt, boxes[b], nx, ny)) {
+                            it = true
+                            break
+                        }
+                    }
+                    if (!it) {
+                        popped = true
+                        
+                        break
+                    } 
+                }
+            }
+        }
+        
+        
+        
+        if (popped) {
+            boxes.push(txt)
+        } else {
+            //alert("Could not add " + word)
+            svg.removeChild(txt)
+        }
+        
     }
     document.body.removeChild(svg)
     return svg
