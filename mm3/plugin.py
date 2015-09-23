4d5386a8d277c86effd485d1145d09c3344158b2
humbedooh <humbedooh@apache.org>
2015-09-23 19:50:50 +0200

    oops, encode, not decode

--- mm3/plugin.py
+++ mm3/plugin.py
@@ -60,7 +60,7 @@ def parse_attachment(part):
             attachment['size'] = len(fd)
             attachment['filename'] = None
             h = hashlib.sha256(fd).hexdigest()
-            b64 = str(base64.urlsafe_b64decode(fd))
+            b64 = str(base64.urlsafe_b64encode(fd))
             attachment['hash'] = h
             for param in dispositions[1:]:
                 key,val = param.split("=")
