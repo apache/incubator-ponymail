c286bd683b7cadd3d441a52354fa7cccc1b0d3da
humbedooh <humbedooh@apache.org>
2015-09-24 11:11:38 +0200

    update import scripts
    - fetch attachments
    - don't auto-update incubator just yet (lot of cruft there)

--- tools/update-asf-lists.sh
+++ tools/update-asf-lists.sh
@@ -3,9 +3,9 @@ DIR=$1
 for tlp in `ls -1 ${DIR}`; do 
     if [ "${tlp}" != "DONE" ]; then
         TLPN=`echo ${tlp} | cut -d. -f1`
-        if [ "${TLPN}" != "${list}" ]; then
+        if [ "${TLPN}" != "incubator" ]; then
           printf "\n*** Updating ${TLPN} ***\n"
-          python import-mbox.py --source "http://mail-archives.eu.apache.org/mod_mbox/" --quick --mod-mbox --project ${TLPN};
+          python import-mbox.py --source "http://mail-archives.eu.apache.org/mod_mbox/" --quick --mod-mbox --project ${TLPN} --attachments;
         fi
     fi
 done
