e97fb62876e4a19835d526c886cf51cf74ffa9ba
humbedooh <humbedooh@apache.org>
2015-10-04 16:01:24 +0200

    slight workaround on the DOM
    innerHTML seems to render too soon, so we have to put the html
    into a temp var and assign once the var is filled.

--- site/js/dev/ponymail_listview_flat.js
+++ site/js/dev/ponymail_listview_flat.js
@@ -83,23 +83,25 @@ function loadList_flat(mjson, limit, start, deep) {
 
 
     var bulk = document.getElementById('emails')
-    bulk.innerHTML = "<div>"
+    bulk.innerHTML = ""
     
     // Top nav buttons
+    var tnav = "<div>"
     if (start > 0) {
         var nstart = Math.max(0, start - limit)
-        bulk.innerHTML += '<div style="width: 33%; float: left;"><a href="javascript:void(0);" style="float: left;" class="btn btn-success" onclick="loadList_flat(false, ' + 15 + ', ' + nstart + ');">Show previous 15</a> &nbsp </div>'
+        tnav += '<div style="width: 33%; float: left;"><a href="javascript:void(0);" style="float: left;" class="btn btn-success" onclick="loadList_flat(false, ' + 15 + ', ' + nstart + ');">Show previous 15</a> &nbsp </div>'
     } else {
-        bulk.innerHTML += '<div style="width: 33%; float: left;">&nbsp;</div>'
+        tnav += '<div style="width: 33%; float: left;">&nbsp;</div>'
     }
     
     if (json.length > (start + limit)) {
         remain = Math.min(15, json.length - (start + limit))
-        bulk.innerHTML += '<div style="width: 33%; float: left;"><a href="javascript:void(0);" style="float: right;" class="btn btn-success" onclick="loadList_flat(false, ' + 15 + ', ' + (start + 15) + ');">Show next ' + remain + '</a></div>'
+        tnav += '<div style="width: 33%; float: left;"><a href="javascript:void(0);" style="float: right;" class="btn btn-success" onclick="loadList_flat(false, ' + 15 + ', ' + (start + 15) + ');">Show next ' + remain + '</a></div>'
     }
-    bulk.innerHTML += "</div>"
+    tnav += "</div>"
     
-    bulk.innerHTML += nest
+    
+    bulk.innerHTML += tnav + nest
     if (prefs.hideStats == 'yes') {
         bulk.setAttribute("class", "well col-md-10 col-lg-10")
     } else {
