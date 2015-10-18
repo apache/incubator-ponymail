75585aea1cf9a51e53c0ec0936d0000e7965c79a
humbedooh <humbedooh@apache.org>
2015-10-18 14:11:22 +0200

    wrong quotes used, sorry!

--- site/js/ponymail.js
+++ site/js/ponymail.js
@@ -1923,7 +1923,7 @@ function buildStats(json, state, show) {
         if (par.name.length == 0) {
             par.name = par.email
         }
-        stats.innerHTML += "<img src='https://secure.gravatar.com/avatar/" + par.gravatar + ".jpg?s=32&r=g&d=mm' style='vertical-align:middle'/>&nbsp;<a href='javascript:void(0)' onclick='do_search(\"" + par.email + "\", '" + current_retention + "')'><b>" + par.name.replace(/[<>]/g, "") + "</a>:</b> " + par.count + " email(s)<br/>";
+        stats.innerHTML += "<img src='https://secure.gravatar.com/avatar/" + par.gravatar + ".jpg?s=32&r=g&d=mm' style='vertical-align:middle'/>&nbsp;<a href='javascript:void(0)' onclick='do_search(\"" + par.email + "\", \"" + current_retention + "\")'><b>" + par.name.replace(/[<>]/g, "") + "</a>:</b> " + par.count + " email(s)<br/>";
     }
 
 
