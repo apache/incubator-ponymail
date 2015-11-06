7c6e13162b6b01cc76395f84a7fe693f44343fe9
humbedooh <humbedooh@apache.org>
2015-11-06 18:39:25 +0100

    Have atom feed also work with threads
    Atom feeds should also be able to show the progress of a single thread,
    independent of which list it's currently on.

--- site/api/atom.lua
+++ site/api/atom.lua
@@ -24,13 +24,64 @@ local aaa = require 'lib/aaa'
 local config = require 'lib/config'
 local cross = require 'lib/cross'
 
+
+function fetchChildren(r, pdoc, c, biglist)
+    c = (c or 0) + 1
+    if c > 250 then
+        return {}
+    end
+    biglist = biglist or {}
+    local children = {}
+    local docs = elastic.find('in-reply-to:"' .. r:escape(pdoc['message-id'])..'"', 50, "mbox")
+    for k, doc in pairs(docs) do
+        if not biglist[doc['message-id']] then
+            biglist[doc['message-id']] = true
+            local mykids = fetchChildren(r, doc, c, biglist)
+            local dc = {
+                private = doc.private,
+                tid = doc.mid,
+                mid = doc.mid,
+                list_raw = doc.list_raw,
+                subject = doc.subject,
+                from = doc.from,
+                id = doc.request_id,
+                epoch = doc.epoch,
+                children = mykids,
+                irt = doc['in-reply-to']
+            }
+            table.insert(children, dc)
+            table.insert(emls_thrd, dc)
+        else
+            docs[k] = nil
+        end
+    end
+    return children
+end
+
+
+function findParent(r, doc)
+    local step = 0
+    while step < 50 do
+        step = step + 1
+        if not doc['in-reply-to'] then
+            break
+        end
+        local docs = elastic.find('message-id:"' .. r:escape(doc['in-reply-to'])..'"', 1, "mbox")
+        if #docs == 0 then
+            break
+        end
+        doc = docs[1]
+    end
+    return doc
+end
+
 function handle(r)
     cross.contentType(r, "application/xhtml+xml")
     local t = {}
     local now = r:clock()
     local tnow = now
     local get = r:parseargs()
-    if not get.list then
+    if not get.list and not get.mid then
         r:puts("<>")
         return cross.OK
     end
@@ -48,71 +99,108 @@ function handle(r)
                     }
                 }
     
-    -- Get threads
-    local threads = {}
-    local emails = {}
-    local emails_full = {}
+    
     local emls = {}
-    local doc = elastic.raw {
-        _source = {'message-id','body','from','subject','epoch','list_raw', 'private'},
-        query = {
-            bool = {
-                must = {
-                    sterm,
-                    {
-                        query_string = {
-                            default_field = "subject",
-                            query = qs
+    
+    -- Get threads from list ID?
+    if get.list then
+        local threads = {}
+        local emails = {}
+        local emails_full = {}
+        local doc = elastic.raw {
+            _source = {'message-id','body','from','subject','epoch','list_raw', 'private'},
+            query = {
+                bool = {
+                    must = {
+                        sterm,
+                        {
+                            query_string = {
+                                default_field = "subject",
+                                query = qs
+                            }
                         }
+                }}
+            },
+            
+            sort = {
+                {
+                    epoch = {
+                        order = "desc"
                     }
-            }}
-        },
+                }  
+            },
+            size = maxresults
+        }
+        local h = #doc.hits.hits
         
-        sort = {
-            {
-                epoch = {
-                    order = "desc"
-                }
-            }  
-        },
-        size = maxresults
-    }
-    local h = #doc.hits.hits
-    
-    -- Debug time point 7
-    table.insert(t, r:clock() - tnow)
-    tnow = r:clock()
-    
-    for k = #doc.hits.hits, 1, -1 do
-        local v = doc.hits.hits[k]
-        local email = v._source
-        local canUse = true
-        if email.private then
-            if account and not rights then
-                rights = aaa.rights(r, account.credentials.uid or account.credentials.email)
+        -- Debug time point 7
+        table.insert(t, r:clock() - tnow)
+        tnow = r:clock()
+        
+        for k = #doc.hits.hits, 1, -1 do
+            local v = doc.hits.hits[k]
+            local email = v._source
+            local canUse = true
+            if email.private then
+                if account and not rights then
+                    rights = aaa.rights(r, account.credentials.uid or account.credentials.email)
+                end
+                canUse = false
+                if account then
+                    local lid = email.list_raw:match("<[^.]+%.(.-)>")
+                    for k, v in pairs(rights or {}) do
+                        if v == "*" or v == lid then
+                            canUse = true
+                            break
+                        end
+                    end
+                end
             end
-            canUse = false
-            if account then
-                local lid = email.list_raw:match("<[^.]+%.(.-)>")
-                for k, v in pairs(rights or {}) do
-                    if v == "*" or v == lid then
-                        canUse = true
-                        break
+            if canUse then
+                local mid = email['message-id']
+                local irt = email['in-reply-to']
+                email.id = v._id
+                email.irt = irt
+                email.references = nil
+                email.to = nil
+                email['in-reply-to'] = nil
+                table.insert(emls, 1, email)
+            end
+        end
+        
+    -- Or get a thread?
+    elseif get.mid then
+        local doc = elastic.get("mbox", get.mid)
+        if doc then
+            local parent = findParent(r, doc)
+            if parent then
+                fetchChildren(r, parent)
+                for k, doc in pairs(emls_thrd) do
+                    local canUse = true
+                    if doc.private then
+                        canUse = false
+                        if account and not rights then
+                            rights = aaa.rights(r, account.credentials.uid or account.credentials.email)
+                        end
+                        if account then
+                            local lid = doc.list_raw:match("<[^.]+%.(.-)>")
+                            for k, v in pairs(rights or {}) do
+                                if v == "*" or v == lid then
+                                    canUse = true
+                                    break
+                                end
+                            end
+                        end
+                    end
+                    if canUse then
+                        table.insert(emls, doc)
                     end
                 end
             end
         end
-        if canUse then
-            local mid = email['message-id']
-            local irt = email['in-reply-to']
-            email.id = v._id
-            email.irt = irt
-            email.references = nil
-            email.to = nil
-            email['in-reply-to'] = nil
-            table.insert(emls, 1, email)
-        end
     end
+    
+    -- Generate the XML
     local scheme = "https"
     if r.port == 80 then
         scheme = "http"
