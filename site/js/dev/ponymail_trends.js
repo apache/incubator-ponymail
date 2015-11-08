d7159837cca743195c26774ed0b18a457f3b5de7
humbedooh <humbedooh@apache.org>
2015-11-08 20:26:15 +0100

    typo + width fix
    it's top 10, not top 25.
    also, make the width conform to the rest if possible

--- site/js/dev/ponymail_trends.js
+++ site/js/dev/ponymail_trends.js
@@ -162,8 +162,8 @@ function showTop(json, state) {
     
     // Top 10 participants
     var top10 = document.createElement('div')
-    top10.setAttribute("style", "margin: 10px; padding: 5px; text-align: left; border-radius: 8px; background: #00C0F1; color: #FFF; font-family: sans-serif; width: 700px;")
-    top10.innerHTML = "<h2 style='margin: 0px; padding: 0px; text-align: left;'><span class='glyphicon glyphicon-star-empty'> </span> Top 25 participants:</h2>"
+    top10.setAttribute("style", "margin: 10px; padding: 5px; text-align: left; border-radius: 8px; background: #00C0F1; color: #FFF; font-family: sans-serif; min-width: 400px;")
+    top10.innerHTML = "<h2 style='margin: 0px; padding: 0px; text-align: left;'><span class='glyphicon glyphicon-star-empty'> </span> Top 10 participants:</h2>"
     
     var l = "<ul>"
     for (var i in json.participants) {
