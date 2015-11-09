69656a70d0b8a3cf637bd9302481ff3d09961dba
humbedooh <humbedooh@apache.org>
2015-11-09 09:27:44 +0100

    Make edit-list.py be able to set list descriptions
    just like what the archiver.py script can do with MM3 lists

--- tools/edit-list.py
+++ tools/edit-list.py
@@ -45,6 +45,7 @@ deleteEmails = None
 wildcard = None
 debug = False
 notag = False
+desc = None
 
 ssl = False
 dbname = config.get("elasticsearch", "dbname")
@@ -71,6 +72,8 @@ parser.add_argument('--source', dest='source', type=str, nargs=1,
                    help='Source list to edit')
 parser.add_argument('--rename', dest='target', type=str, nargs=1,
                    help='(optional) new list ID')
+parser.add_argument('--desc', dest='desc', type=str, nargs=1,
+                   help='(optional) new list description')
 parser.add_argument('--private', dest='private', action='store_true', 
                    help='Make all emails in list private')
 parser.add_argument('--public', dest='public', action='store_true', 
@@ -90,6 +93,8 @@ if args.source:
     sourceLID = args.source[0]
 if args.target:
     targetLID = args.target[0]
+if args.desc:
+    desc = args.desc[0]
 if args.private:
     makePrivate = args.private
 if args.public:
@@ -102,12 +107,13 @@ if args.debug:
     debug = args.debug
 if args.notag:
     notag = args.notag
+
     
 if not sourceLID:
     print("No source list ID specified!")
     parser.print_help()
     sys.exit(-1)
-if not (targetLID or makePrivate or makePublic or deleteEmails):
+if not (targetLID or makePrivate or makePublic or deleteEmails or desc):
     print("Nothing to do! No target list ID or action specified")
     parser.print_help()
     sys.exit(-1)
@@ -133,65 +139,80 @@ if deleteEmails:
 
 count = 0
 
-
-print("Updating docs...")
-then = time.time()
-page = es.search(
-    index=dbname,
-    doc_type="mbox",
-    scroll = '30m',
-    search_type = 'scan',
-    size = 100,
-    body = {
-        'query': {
-            'bool': {
-                'must': [
-                    {
-                        'wildcard' if wildcard else 'term': {
-                            'list_raw': sourceLID
+if desc:
+    LID = sourceLID
+    if targetLID:
+        LID = targetLID
+    self.es.index(
+        index=dbname,
+        doc_type="mailinglists",
+        id=LID,
+        body = {
+            'list': LID,
+            'name': LID,
+            'description': desc
+        }
+    )
+    
+if targetLID or makePrivate or makePublic or deleteEmails:
+    print("Updating docs...")
+    then = time.time()
+    page = es.search(
+        index=dbname,
+        doc_type="mbox",
+        scroll = '30m',
+        search_type = 'scan',
+        size = 100,
+        body = {
+            'query': {
+                'bool': {
+                    'must': [
+                        {
+                            'wildcard' if wildcard else 'term': {
+                                'list_raw': sourceLID
+                            }
                         }
-                    }
-                ]
+                    ]
+                }
             }
         }
-    }
-    )
-sid = page['_scroll_id']
-scroll_size = page['hits']['total']
-if debug:
-        print(json.dumps(page))
-js_arr = []
-while (scroll_size > 0):
-    page = es.scroll(scroll_id = sid, scroll = '30m')
-    if debug:
-        print(json.dumps(page))
+        )
     sid = page['_scroll_id']
-    scroll_size = len(page['hits']['hits'])
-    for hit in page['hits']['hits']:
-        doc = hit['_id']
-        body = {}
-        if targetLID:
-            body['list_raw'] = targetLID
-            body['list'] = targetLID
-        if makePrivate:
-            body['private'] = True
-        if makePublic:
-            body['private'] = False
-        js_arr.append({
-            '_op_type': 'delete' if deleteEmails else 'update',
-            '_index': dbname,
-            '_type': 'mbox',
-            '_id': doc,
-            'doc': body
-        })
-        
-        count += 1
-        if (count % 500 == 0):
-            print("Processed %u emails..." % count)
-            helpers.bulk(es, js_arr)
-            js_arr = []
-
-if len(js_arr) > 0:
-    helpers.bulk(es, js_arr)
+    scroll_size = page['hits']['total']
+    if debug:
+            print(json.dumps(page))
+    js_arr = []
+    while (scroll_size > 0):
+        page = es.scroll(scroll_id = sid, scroll = '30m')
+        if debug:
+            print(json.dumps(page))
+        sid = page['_scroll_id']
+        scroll_size = len(page['hits']['hits'])
+        for hit in page['hits']['hits']:
+            doc = hit['_id']
+            body = {}
+            if targetLID:
+                body['list_raw'] = targetLID
+                body['list'] = targetLID
+            if makePrivate:
+                body['private'] = True
+            if makePublic:
+                body['private'] = False
+            js_arr.append({
+                '_op_type': 'delete' if deleteEmails else 'update',
+                '_index': dbname,
+                '_type': 'mbox',
+                '_id': doc,
+                'doc': body
+            })
             
-print("All done, processed %u docs in %u seconds" % (count, time.time() - then))
+            count += 1
+            if (count % 500 == 0):
+                print("Processed %u emails..." % count)
+                helpers.bulk(es, js_arr)
+                js_arr = []
+    
+    if len(js_arr) > 0:
+        helpers.bulk(es, js_arr)
+                
+    print("All done, processed %u docs in %u seconds" % (count, time.time() - then))
