db8cc81edba852a523bf65fb4de1f94d0719ac83
humbedooh <humbedooh@apache.org>
2015-11-13 07:48:23 +0100

    also update date query here
    oops

--- site/api/stats.lua
+++ site/api/stats.lua
@@ -382,7 +382,7 @@ function handle(r)
                         {
                         range = {
                             date = {
-                                gt = "0",
+                                gt = "1970/01/01 00:00:00",
                             }
                         }
                     },sterm
