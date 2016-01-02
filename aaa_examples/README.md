154801bd8567210bcfc80b22ecbf420ef1004c01
humbedooh <humbedooh@apache.org>
2016-01-02 12:25:06 +0100

    Add an AAA example

---
+++ aaa_examples/README.md
@@ -0,0 +1,10 @@
+# AAA Examples
+This directory contains example AAA libraries for various use cases
+
+### AAA by email address:
+[`aaa_by_email_address.lua`](aaa_by_email_address.lua) checks against a GLOB
+(`valid_email`), and if a logged-in user's email address matches this, provides
+access to private lists, provided the OAuth provider used is listed in
+`config.lua` as a valid authority.
+
+
---
+++ aaa_examples/aaa_by_email_address.lua
@@ -0,0 +1,83 @@
+--[[
+ Licensed to the Apache Software Foundation (ASF) under one or more
+ contributor license agreements.  See the NOTICE file distributed with
+ this work for additional information regarding copyright ownership.
+ The ASF licenses this file to You under the Apache License, Version 2.0
+ (the "License"); you may not use this file except in compliance with
+ the License.  You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+]]--
+
+-- This is aaa.lua - AAA filter with email validation.
+-- It checks that an oauth validated email account matches *@foocorp.com and if
+-- so, grants access to private emails.
+-- To use this as your AAA lib, replace aaa.lua in site/api/lib with this file.
+
+local config = require 'lib/config'
+
+-- validated emails ending with @foocorp.com have access to all private emails
+-- This is a GLOB, so could also be *@internal.foocorp.com, or *-admin@foocorp.com etc
+local valid_email = "*@foocorp.com" 
+
+
+-- Is email a valid foocorp email?
+function validateEmail(r, email)
+    -- do a GLOB match, testing email aginst valid_email
+    if r:strcmp_match(valid_email, email) then
+        return true
+    end
+    return false
+end
+
+-- Get a list of domains the user has private email access to (or wildcard if org member)
+function getRights(r, usr)
+    if not usr.credentials then
+        return {}
+    end
+    local email = usr.credentials.email or "|||"
+    local xemail = email:match("([-a-zA-Z0-9._@]+)") -- whitelist characters
+    local rights = {}
+    
+    -- bad char in email?
+    if not email or exmail ~= email then
+        return rights
+    end
+    
+    -- Check that we used oauth, bail if not
+    local oauth_domain = usr.internal and usr.internal.oauth_used or nil
+    if not oauth_domain then
+        return {}
+    end
+    
+    -- check if oauth was through an oauth portal that can give privacy rights
+    local authority = false
+    for k, v in pairs(config.admin_oauth or {}) do
+        if r.strcmp_match(oauth_domain, v) then
+            authority = true
+            break
+        end
+    end
+    
+    -- if not a 'good' oauth, then let's forget all about it
+    if not authority then
+        return rights
+    end
+    
+    -- Check if email matches foocorp.com
+    if usr.internal.admin or validateEmail(r, email) then
+        table.insert(rights, "*")
+    end
+    return rights
+end
+
+-- module defs
+return {
+    rights = getRights
+}
