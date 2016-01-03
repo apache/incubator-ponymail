164414abde7cb5c9b68a64944af6923393529994
humbedooh <humbedooh@apache.org>
2016-01-03 19:11:26 +0100

    nest to a maximum of 20 levels in treeview
    otherwise it may become absurd..

--- site/js/ponymail.js
+++ site/js/ponymail.js
@@ -2677,7 +2677,8 @@ function buildTreeview(nesting, list, obj, pbigger) {
     var now = new Date().getTime() / 1000
     for (var i in list) {
         var nvi = ""
-        for (var z = 1; z <= nesting; z++) {
+        // We'll nest to a max depth of 20, to not explode everything
+        for (var z = 1; z <= Math.max(nesting, 20); z++) {
             if (z == nesting) {
                 if (i == (list.length -1)) {
                     nvi += "<img src='/images/treeview_lastchild.png' style='height: 40px; width: 16px; overflow: hidden; margin-top: -5px;'/>"
