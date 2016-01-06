7e6f614f8f6109fb3da318d472fd06b4bf707ffe
humbedooh <humbedooh@apache.org>
2016-01-06 11:13:28 +0100

    add a few comments

--- site/js/dev/ponymail_assign_vars.js
+++ site/js/dev/ponymail_assign_vars.js
@@ -19,22 +19,22 @@
 // They keep track of the JSON we have received, storing it in the browser,
 // Thus lightening the load on the backend (caching and such)
 
-var _VERSION_ = "0.8b"
+var _VERSION_ = "0.8b" // Current version (as far as we know)
 var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 var d_ppp = 15; // results per page
 var c_page = 0; // current page position for list view
-var open_emails = []
+var open_emails = [] // cache index for loaded emails
 var list_year = {}
 var current_retention = "lte=1M" // default timespan for list view
 var current_cal_min = 1997 // don't go further back than 1997 in case everything blows up, date-wise
 var keywords = ""
-var current_thread = 0
-var current_thread_mids = {}
-var saved_emails = {}
-var current_query = ""
-var old_json = {}
+var current_thread = 0 // marker for list view; currently open thread/email
+var current_thread_mids = {} // duplicate guard for threading
+var saved_emails = {} // JSON cache for emails
+var current_query = "" // currently active search query
+var old_json = {} // pointer to previously loaded JSON object
 var all_lists = {}
-var current_json = {}
+var current_json = {} // pointer to currently loaded JSON
 var current_thread_json = {}
 var current_flat_json = {}
 var current_email_msgs = []
@@ -54,8 +54,8 @@ var latestEmailInThread = 0
 var composeType = "reply"
 var gxdomain = ""
 var fl = null
-var kiddos = []
-var pending_urls = {}
+var kiddos = [] // DOM tree for traverse functions
+var pending_urls = {} // URL list for GetAsync's support functions (such as the spinner)
 var pb_refresh = 0
 var treeview_guard = {}
 var mbox_month = null
