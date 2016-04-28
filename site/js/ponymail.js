ec431f43ba5e3c7beb724045a27112e12cad0fce
humbedooh <humbedooh@apache.org>
2016-04-28 22:39:14 +0200

    comment out this button
    that seems to not work...

--- site/js/ponymail.js
+++ site/js/ponymail.js
@@ -1711,6 +1711,8 @@ function toggleEmails_threaded(id, close, toverride, threadobj) {
         var ml = findEml(current_thread_json[id].tid)
         if (!current_thread_json[id].magic && ml.irt && ml.irt.length > 0) {
             helper.innerHTML += "<p id='magic_"+id+"'><i><b>Note:</b> You are viewing a search result/aggregation in threaded mode. Only results matching your keywords or dates are shown, which may distort the thread. For the best result, go to the specific list and view the full thread there, or view your search results in flat mode. Or we can <a href='javascript:void(0);' onclick='timeTravelList("+id+")'>do some magic for you</a>.</i></p>"
+            // Why was this here??
+            /*
             var btn = document.createElement('a')
             btn.setAttribute("href", "javascript:void(0);")
             btn.setAttribute("class", "btn btn-success")
@@ -1718,6 +1720,7 @@ function toggleEmails_threaded(id, close, toverride, threadobj) {
             btn.style.marginRight = "10px"
             btn.innerHTML = "View results in flat mode instead"
             helper.appendChild(btn)
+            */
         } else if (!current_thread_json[id].magic) {
             helper.innerHTML += "<p id='magic_"+id+"'></p>"
         }
