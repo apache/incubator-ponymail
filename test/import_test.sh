9141d6d3727a851368f1a8c4b05bd8eee4f5a2bb
Sebb <sebb@apache.org>
2016-11-21 21:58:57 +0000

    Divide valid tests into ones which pass and ones which don't yet

--- test/import_test.sh
+++ test/import_test.sh
@@ -2,12 +2,19 @@
 
 # Test file importing
 
-echo ++++++++++++ Testing failures ++++++++++++
+echo ++++++++++++ Testing imports that should fail ++++++++++++
 ../tools/import-mbox.py --dry --duplicates --source resources/fail
 echo ------------ Should be zero records inserted --------------
 echo
 
 COUNT=$(grep '^From ' resources/pass/*.mbox | wc -l)
-echo ++++++++++++ Testing passes ++++++++++++
+echo ++++++++++++ Testing imports that should pass ++++++++++++
 ../tools/import-mbox.py --dry --duplicates --source resources/pass
 echo ------------ Expecting $COUNT records inserted/updated and 0 bad records --------------
+echo
+
+COUNT=$(grep '^From ' resources/valid/*.mbox | wc -l)
+echo "++++++++++++ Testing imports that should pass but don't currently ++++++++++++"
+../tools/import-mbox.py --dry --duplicates --source resources/valid
+echo ------------ Expecting 0 records inserted/updated and $COUNT bad records --------------
+echo
