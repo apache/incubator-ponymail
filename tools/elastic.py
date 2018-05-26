b5e70ce4e8b80a0d8f462b5471d10961dc7c1030
Sebb <sebb@apache.org>
2018-05-26 13:10:14 +0100

    Use specific error

--- tools/elastic.py
+++ tools/elastic.py
@@ -26,7 +26,7 @@ import logging
 import certifi
 
 try:
-    from elasticsearch import Elasticsearch, helpers, ElasticsearchException
+    from elasticsearch import Elasticsearch, helpers, ConnectionError
     from elasticsearch import VERSION as ES_VERSION
 except ImportError as e:
     sys.exit("Sorry, you need to install the elasticsearch module from pip first. (%s)" % str(e))
@@ -76,7 +76,7 @@ class Elastic:
         if not self.dbVersion:
             try:
                 self.dbVersion = self.info()['version']['number']
-            except ElasticsearchException:
+            except ConnectionError:
                 # default if cannot connect; allows retry
                 return '0.0.0'
         return self.dbVersion
